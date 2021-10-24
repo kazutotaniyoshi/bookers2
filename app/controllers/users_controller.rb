783f8933ce8cbc6d78ff4652a4d1d3f84ad8d796
EC2 Default User <ec2-user@ip-172-31-93-223.ec2.internal>
2021-10-24 17:26:04 +0000

    完成しました

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
     if @user == current_user
         render :edit
     else
-        redirect_to books_path
+        redirect_to user_path(current_user)
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,6 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   attachment :profile_image
 
-  validates :name, presence: true,length: { minimum: 2, maximum: 20 },uniqueness: { case_sensitive: false }
-  validates :email, presence: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
-  validates :introduction,length: { maximum: 50 }
+ validates :name,  uniqueness: true,  length: { minimum: 2, maximum: 20 }
+ validates :introduction, length: { maximum: 50 }
 end
